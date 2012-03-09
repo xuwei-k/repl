cd3aac3943d38288166cfc7e05919f3f0c845bb2
xuwei-k <6b656e6a69@gmail.com>
2012-03-10 01:21:59 +0900

    implement conscript

--- cs/src/main/scala/App.scala
+++ cs/src/main/scala/App.scala
@@ -1,5 +1,8 @@
 package com.github.xuwei_k.repl
 
+import sbt._
+import scala.util.control.Exception.allCatch
+
 class App extends xsbti.AppMain {
   def run(config: xsbti.AppConfiguration) = {
     Exit(App.run(config.arguments))
@@ -7,10 +10,32 @@ class App extends xsbti.AppMain {
 }
 
 object App {
+  object FileExtractor{
+    def unapply(str:String):Option[File] = {
+      val f = file(str)
+      if( f.exists && f.isFile ) Some(f)
+      else None
+    }
+  }
 
   def run(args: Array[String]): Int = {
-    println("Hello World: " + args.mkString(" "))
-    0
+    args match{
+      case null => System.err.println("args is Empty") ; -1
+      case Array(FileExtractor(f),opt @ _*) => run0(f,opt) ; 0
+      case _ => System.err.println("bad options: " + args.mkString(" ")); -1
+    }
+  }
+
+  // TODO should not use other process !
+  def run0(f:File,option:Seq[String] = Nil){
+    val str = Core.generate(f,option,Nil)
+    println(str)
+    IO.withTemporaryFile("repl",".scala"){ file =>
+      IO.write(file,str + "\n\n" + f.getName.replace(".scala","") + " main null" )
+      val cmd = "scala " + file.toString
+      println(cmd)
+      cmd.lines_! foreach println
+    }
   }
 
   def main(args: Array[String]) {
